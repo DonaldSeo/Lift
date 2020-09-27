f8e5071b7498166d36db25956f3b73f8f3770955
Donald Seo <deokjae.seo@gmail.com>
2020-09-26 23:29:47 -0400

    added actionsheet in exercise list view with completion handler

--- Lift/Controller/ExerciseListVC.swift
+++ Lift/Controller/ExerciseListVC.swift
@@ -13,6 +13,8 @@ import Firebase
 class ExerciseListVC: UIViewController {
   
   var selectedCategoryId: Int?
+  var selectedWorkoutDay: Int?
+  let workoutDays = ["Unsorted", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
   var selectedCategoryName: String = ""
   var exerciseList: [Exercise] = []
   var user: User!
@@ -39,20 +41,36 @@ class ExerciseListVC: UIViewController {
     getExerciseListFromCategory()
     
   }
+  func displayWorkoutDaySelectSheet(completion: @escaping (Int) -> ()) {
+    let alert = UIAlertController(title: "Select the workout day", message: "Please Select an Option", preferredStyle: .actionSheet)
+    
+    for dayIndex in 0..<workoutDays.count {
+      alert.addAction(UIAlertAction(title: workoutDays[dayIndex], style: .default , handler:{ _ in
+        completion(dayIndex)
+      }))
+    }
+    alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler:{ (UIAlertAction)in
+        print("User click Dismiss button")
+    }))
+
+    self.present(alert, animated: true, completion: nil)
+    
+  }
+  
   func displaySuccessAlert() {
     let alert = UIAlertController(title: "Success", message: "Exercise Added to your workout list", preferredStyle: UIAlertController.Style.alert)
-      alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
-          switch action.style{
-          case .default:
-              print("default")
+    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+        switch action.style{
+        case .default:
+            print("default")
 
-          case .cancel:
-              print("cancel")
+        case .cancel:
+            print("cancel")
 
-          case .destructive:
-              print("destructive")
+        case .destructive:
+            print("destructive")
 
-          }}))
+        }}))
       self.present(alert, animated: true, completion: nil)
       DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
 
@@ -78,9 +96,9 @@ class ExerciseListVC: UIViewController {
     }
   }
   
-  func addExerciseToWorkoutPlan(at indexPath: IndexPath) {
+  func addExerciseToWorkoutPlan(at indexPath: IndexPath, day workoutDaySelection: Int) {
     // TODO: - update firebase workout list with selected exercise
-    let exercise : [String: Any] = ["name" : exerciseList[indexPath.row].name]
+    let exercise : [String: Any] = ["name" : exerciseList[indexPath.row].name, "workoutSection" : workoutDaySelection]
     let userWorkoutRef = rootReference.child("Workout").child(user.uid).child("List").childByAutoId()
 //    let userWorkoutRef = rootReference.child("Workout-List/\(user.uid)/name")
     userWorkoutRef.setValue(exercise)
@@ -101,11 +119,37 @@ extension ExerciseListVC: UITableViewDelegate, UITableViewDataSource {
   
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     // here call addExerciseToWorkoutPlan()
-    
-    addExerciseToWorkoutPlan(at: indexPath)
-    displaySuccessAlert()
+//    TODO: ADD actionSheet type alert and let user choose workout day for selected exercise
+    displayWorkoutDaySelectSheet() { daySelection in
+      self.addExerciseToWorkoutPlan(at: indexPath, day: daySelection)
+      self.displaySuccessAlert()
+    }
+
+
     tableView.deselectRow(at: indexPath, animated: true)
   }
   
   
+}
+
+extension ExerciseListVC: UIPickerViewDelegate, UIPickerViewDataSource {
+  func numberOfComponents(in pickerView: UIPickerView) -> Int {
+      return 2
+  }
+
+  func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+      if component == 0 {
+          return 10
+      } else {
+          return 100
+      }
+  }
+
+  func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+      if component == 0 {
+          return "First \(row)"
+      } else {
+          return "Second \(row)"
+      }
+  }
 }
